e89475c618bdc2916198254459b83832f0bd527a
JonathonStyles <18717323@student.westernsydney.edu.au>
2018-08-22 21:50:47 +1000

    Added more to the landing page and added part of the registration page. Note: No ability to switch between pages yet.

---
+++ app/src/main/java/com/example/kirmi/ks1807/Register.java
@@ -0,0 +1,15 @@
+package com.example.kirmi.ks1807;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
