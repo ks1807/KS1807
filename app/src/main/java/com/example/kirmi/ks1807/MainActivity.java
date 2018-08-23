d7ae383b7b0b30db1d4aa29adbc5d3202ede6a7f
JonathonStyles <18717323@student.westernsydney.edu.au>
2018-08-23 21:28:39 +1000

    Can now switch between pages, added more to register page.

--- app/src/main/java/com/example/kirmi/ks1807/MainActivity.java
+++ app/src/main/java/com/example/kirmi/ks1807/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.kirmi.ks1807;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void button_Register(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, Register.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/kirmi/ks1807/Register.java
+++ app/src/main/java/com/example/kirmi/ks1807/Register.java
@@ -1,7 +1,9 @@
 package com.example.kirmi.ks1807;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Register extends AppCompatActivity
 {
@@ -11,5 +13,11 @@ public class Register extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void button_Back(View view)
+    {
+        Intent intent = new Intent(Register.this, MainActivity.class);
+        startActivity(intent);
     }
 }
