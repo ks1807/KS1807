39075e026b04faf0d14ebb80bffb5a5c855ed7cd
nullarmo <nullarmo@gmail.com>
2018-10-10 08:46:17 +1100

    Added error messages to the Spotify login button Made service start on app launch

---
+++ app/src/main/java/com/example/kirmi/ks1807/Overlay.java
@@ -0,0 +1,13 @@
+package com.example.kirmi.ks1807;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Overlay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overlay);
+    }
+}
